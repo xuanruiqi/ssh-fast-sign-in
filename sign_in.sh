393e53199c0145aa0a2fe5843bed37daefa77f56
Xuanrui Qi <xuanruiqi@users.noreply.github.com>
2016-03-03 15:11:43 -0500

    Improve script

--- sign_in.sh
+++ sign_in.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 if [ -z "$SSH_USER" ]; then
-	echo "SSH_USER is not defined. Unable to ssh into your Tufts account."
-	echo "Export SSH_USER in your .bashrc or .profile and re-login."
+	echo "SSH_USER is not defined. Unable to ssh into your account."
+	echo "Run the install script and re-login."
+elif [ -z "$SSH_DOMAIN" ]; then
+	echo "SSH_DOMAIN is not defined. Unable to ssh into your account."
+	echo "Run the install script and re-login."
 else
 	printf "ssh-ing into %s@%s..." "$SSH_USER" "$SSH_DOMAIN"
 	ssh -X $SSH_USER@$SSH_DOMAIN
