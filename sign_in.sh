54736d27e864e106ce39cff7a183264a885de01d
Xuanrui Qi <xuanruiqi@users.noreply.github.com>
2016-03-03 01:57:24 -0500

    Edit typos

--- sign_in.sh
+++ sign_in.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ -z "$SSH_USER" ]; then
-	echo "TUFTS_USER is not defined. Unable to ssh into your Tufts account."
-	echo "Export TUFTS_USER in your .bashrc or .profile and re-login."
+	echo "SSH_USER is not defined. Unable to ssh into your Tufts account."
+	echo "Export SSH_USER in your .bashrc or .profile and re-login."
 else
 	printf "ssh-ing into %s@%s..." "$SSH_USER" "$SSH_DOMAIN"
 	ssh -X $TUFTS_USER@$SSH_DOMAIN
